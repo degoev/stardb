615d5c46011dd02a4bb1418c424af58ad1c27862
degoev <50908619+degoev@users.noreply.github.com>
2020-03-15 00:46:07 +0300

    добавил обработку ошибок в random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -4,47 +4,62 @@ import "./random-planet.css";
 import SwapiService from "../../services/swapi-service";
 import Spinner from "../spinner/spinner";
 
+import Error from "../error-indicator/error-indicator";
+
 export default class RandomPlanet extends Component {
     constructor() {
         super();
 
         this.updatePlanet();
 
+        //setInterval(async () => await this.updatePlanet(), 5000);
+
         this.state = {
             planet: {},
-            loading: true
+            loading: true,
+            error: false
         };
     }
 
     swapiService = new SwapiService()
 
+    onError = () => {
+        this.setState({ error: true, loading: false })
+    };
+
     onPlanetLoaded = (planet) => {
         this.setState({ planet, loading: false })
     };
 
-    updatePlanet() {
+    updatePlanet = () => {
+        console.log("update")
         let id = Math.floor(Math.random() * 18) + 2;
         this.swapiService
             .getPlanet(id)
-            .then(this.onPlanetLoaded);
+            .then(this.onPlanetLoaded)
+            .catch(this.onError);
     }
 
     render() {
         let { planet,
-            loading } = this.state;
+            loading,
+            error } = this.state;
 
+        let hasData = !(loading || error);    
+        let err = error ? <Error /> : null;
         let spinner = loading ? <Spinner /> : null;
-        let content = !loading ? <PlanetView planet={planet} /> : null;
+        let content = hasData ? <PlanetView planet={planet} /> : null;
         return (
             <div className="random-planet jumbotron rounded">
-                { spinner }
-                { content }
+                {err}
+                {spinner}
+                {content}
             </div>
         );
     }
 };
 
-let PlanetView = ( {planet} ) => {
+let PlanetView = ({ planet }) => {
     let { id, name, population,
         rotationPeriod, diameter } = planet;
     return (
@@ -53,9 +68,9 @@ let PlanetView = ( {planet} ) => {
             <div>
                 <h3>{name}</h3>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">Population <span>{population}</span></li>
-                    <li className="list-group-item">Rotation period <span>{rotationPeriod}</span></li>
-                    <li className="list-group-item">Diameter <span>{diameter}</span></li>
+                    <li className="list-group-item">Population: <span>{population}</span></li>
+                    <li className="list-group-item">Rotation period: <span>{rotationPeriod}</span></li>
+                    <li className="list-group-item">Diameter: <span>{diameter}</span></li>
                 </ul>
             </div>
         </React.Fragment>
